11b36752475505d5d18bb45661a1e5880fe2e305
ThePrabhu <prabhupaloji2008@gmail.com>
2025-12-29 10:55:59 +0530

    FIles edited

--- basic_practice_problems/scripts.js
+++ basic_practice_problems/scripts.js
@@ -1,6 +1,1 @@
-let gameNum = 45;
-let userNum = prompt("Guess the number : ")
-while (userNum != gameNum){
-    userNum = prompt("You guess a wrong number, try guessing again : ")
-}
-console.log("congratulations your guess was right!")
+
