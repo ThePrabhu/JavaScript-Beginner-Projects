fdd76cbbd46a205e3afd381c47c3090814454383
ThePrabhu <prabhupaloji2008@gmail.com>
2026-01-22 13:51:09 +0530

    Persisted Toggle was added

--- locastorage/script.js
+++ locastorage/script.js
@@ -1,17 +1,36 @@
-// variables
+// selectors
+const main = document.querySelector(".main_page");
 const off = document.querySelector(".fa-toggle-off");
 const on = document.querySelector(".fa-toggle-on");
-const main = document.querySelector(".main_page");
 
-
-
-// function to perform the action
-function state(){
-    off.classList.toggle("hideClass");
-    on.classList.toggle("hideClass");
-    main.classList.toggle("themeChange");
+// ---------- STATE ----------
+function applyTheme(theme) {
+  if (theme === "dark") {
+    main.classList.add("themeChange");
+    off.classList.add("hideClass");
+    on.classList.remove("hideClass");
+  } else {
+    main.classList.remove("themeChange");
+    off.classList.remove("hideClass");
+    on.classList.add("hideClass");
+  }
 }
 
-// Event Listenrs for toggles
-off.addEventListener("click" , state)
-on.addEventListener("click" , state)
+// ---------- SAVE STATE ----------
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+  applyTheme(theme);
+}
+
+// ---------- LOAD STATE ON PAGE LOAD ----------
+const savedTheme = localStorage.getItem("theme") || "light";
+applyTheme(savedTheme);
+
+// ---------- EVENTS ----------
+off.addEventListener("click", () => {
+  setTheme("dark");
+});
+
+on.addEventListener("click", () => {
+  setTheme("light");
+});
